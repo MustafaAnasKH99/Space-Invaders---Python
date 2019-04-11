7c5105da42e3922f6cd4d6522588a1092a94e784
Mustafa Anas <ad16mkha@uwcad.it>
2019-04-11 22:26:47 +0300

    Firing Implemented

--- space_invaders/game.py
+++ space_invaders/game.py
@@ -31,16 +31,29 @@ player.setheading(90)
 
 #Initiate Enemy
 enemy = turtle.Turtle()
-enemy.setposition(0, 250)
+enemy.shape("circle")
+enemy.setposition(-200, 250)
 enemy.color('red')
-enemy.shape('triangle')
 enemy.penup()
-enemy.speed(0)
+enemy.speed(2)
 enemy.setheading(270)
 
 #Set Speed
 player_speed = 15
 enemy_speed = 15
+
+#Create the Player's Weapon:
+bullet = turtle.Turtle()
+bullet.color('yellow')
+bullet.shape('triangle')
+bullet.penup()
+bullet.speed(0)
+bullet.setheading(90)
+bullet.shapesize(0.5, 0.5)
+bullet.hideturtle()
+bullet_speed = 20
+bullet_state = 'ready'
+
 
 
 # Player Motion
@@ -68,30 +81,14 @@ def move_player_down():
     y -= player_speed
     player.sety(y)
 
-# Enemy Motion:
-def move_enemy_left():
-    x = enemy.xcor()
-    x -= enemy_speed
-    if x < -280:
-        x = -280
-    enemy.setx(x)
-
-def move_enemy_right():
-    x = enemy.xcor()
-    x += enemy_speed   
-    if x > 280: 
-        x = 280
-    enemy.setx(x)
-
-def move_enemy_down():
-    y = enemy.ycor()
-    y -= enemy_speed
-    enemy.sety(y)
-
-def move_enemy_up():
-    y = enemy.ycor()
-    y += enemy_speed
-    enemy.sety(y)
+def fire_bullet():
+    global bullet_state
+    if bullet_state == 'ready':
+        bullet_state = 'fire'
+        x = player.xcor()
+        y = player.ycor() + 10
+        bullet.setposition(x, y)
+        bullet.showturtle()
 
 
 #Events Listeners
@@ -100,11 +97,37 @@ turtle.onkey(move_player_left, 'a')
 turtle.onkey(move_player_right, 'd')
 turtle.onkey(move_player_up, 'w')
 turtle.onkey(move_player_down, 's')
+turtle.onkey(fire_bullet, 'space')
 
-turtle.onkey(move_enemy_left, 'Left')
-turtle.onkey(move_enemy_right, 'Right')
-turtle.onkey(move_enemy_up, 'Up')
-turtle.onkey(move_enemy_down, 'Down')
+# Enemy Motion / Main Loop:
+while True:
+    #Move Enemy
+    x = enemy.xcor()
+    x += enemy_speed
+    enemy.setx(x)
 
+    if enemy.xcor() > 280:
+        y = enemy.ycor()
+        y -= 40
+        enemy_speed *= -1
+        enemy.sety(y)
+
+    if enemy.xcor() < -280:
+        y = enemy.ycor()
+        y -= 40
+        enemy_speed *= -1
+        enemy.sety(y)
+
+    if bullet_state == 'fire':
+        y = bullet.ycor()
+        y += bullet_speed
+        bullet.sety(y)
+
+    if bullet.ycor() > 275:
+        bullet.hideturtle()
+        bullet_state = 'ready'
+
+    if bullet.ycor() == enemy.ycor():
+        print('won!')
 
 turtle.done()
