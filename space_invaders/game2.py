b9681d6ae68a2bb10b4e27f4c340e3c62b909b46
Mustafa Anas <khmostafa4@gmail.com>
2019-11-27 19:52:14 -0500

    update game2.py

--- space_invaders/game2.py
+++ space_invaders/game2.py
@@ -1,41 +1,14 @@
 import turtle
-import random
+pen = turtle.Turtle()
 
-window = turtle.Screen()
-window.bgcolor('black')
-window.title('Space Invaders')
-
-border_pen = turtle.Turtle()
-border_pen.speed(0)
-border_pen.color('white')
-border_pen.penup()
-border_pen.setposition(-300,-300)
-border_pen.pendown()
-border_pen.pensize(3)
-
-for side in range(4):
-    border_pen.fd(600)
-    border_pen.lt(90)
-    border_pen.hideturtle()
+pen.pendown() #this line puts the pen on the paper
+for side in range(3): #see number three? its what reminds Python,  THREE times!
+  pen.forward(100)
+  pen.right(90)
+pen.forward(100)
 
 player = turtle.Turtle()
-player.setposition(0, -250)
-player.color('blue')
-player.shape('triangle')
 player.penup()
-player.speed(0)
-player.setheading(90)
-
-enemies = []
-for i in range(5):
-    enemies.append(turtle.Turtle())
-
-for enemy in enemies:
-    enemy.penup()
-    enemy.color('yellow')
-    x = random.randint(-200, 200)
-    y = random.randint(100, 250)
-    enemy.setposition(x, y)
 
 def moveRight():
     x = player.xcor()
@@ -45,23 +18,25 @@ def moveRight():
 def moveLeft():
     x = player.xcor()
     x -= 10
-    player.setx (x)
+    player.setx(x)  
 
-def moveUp():
+def moveForward():
+    print('something')
     y = player.ycor()
     y += 10
     player.sety(y)
 
-def moveDown():
+def moveBackward():
     y = player.ycor()
     y -= 10
     player.sety(y)
 
-turtle.listen()
-turtle.onkey(moveRight, 'Right')
-turtle.onkey(moveLeft, 'Left')
-turtle.onkey(moveDown, 'Down')
-turtle.onkey(moveUp, 'Up')
+wn = turtle.Screen()
+wn.listen()
 
-turtle.done()
+wn.onkey(moveRight, 'd')
+wn.onkey(moveLeft, 'a')
+wn.onkey(moveForward, 'w')
+wn.onkey(moveBackward, 's')
 
+turtle.done() 
